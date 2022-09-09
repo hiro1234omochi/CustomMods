c0c4599a34754c5ea130c882e1adb936f9383a62
hiro1234omochi <hiromochi01234@outlook.jp>
2022-09-09 19:35:50 +0900

    ・2.2-SNAPSHOT ・爆発のブロック破壊を無効にする対象を変更できる機能を追加。 ・setconfigコマンドで爆発のブロック破壊を無効にする対象を変更できる機能を追加。 ・/help setconfig コマンドで出力されるときのexplosionのhelpを追加。 ・help コマンドで出力されるときにusageタグ以外のものも出力されてしまう不具合を修正。

--- src/main/java/com/omochiserver/custommods/commands/SetConfig.java
+++ src/main/java/com/omochiserver/custommods/commands/SetConfig.java
@@ -22,21 +22,53 @@ public class SetConfig implements CommandExecutor
             sender.sendMessage(ChatColor.RED + "引数が足りません。詳しくは /help setconfig を参照してください。");
             return true;
         }
-        if(!(args[0].equalsIgnoreCase("fly") || args[0].equalsIgnoreCase("nickname")))
+        if(!(args[0].equalsIgnoreCase("fly") || args[0].equalsIgnoreCase("nickname") || args[0].equalsIgnoreCase("explosion")))
         {
             sender.sendMessage(ChatColor.RED + "引数の1番目が間違っています。詳しくは /help setconfig を参照してください");
             return true;
         }
-        if(!(args[1].equalsIgnoreCase("op") || args[1].equalsIgnoreCase("notop") || args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("false")))
+        if(!(args[1].equalsIgnoreCase("op") || args[1].equalsIgnoreCase("notop") || args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("false")
+                || args[1].equalsIgnoreCase("sensing_radius") || args[1].equalsIgnoreCase("sensing_fire")))
         {
             sender.sendMessage(ChatColor.RED + "引数の2番目が間違っています。詳しくは /help setconfig を参照してください");
             return true;
         }
-        sender.sendMessage(ChatColor.GREEN + "[config]" + args[0] + "に必要な権限を" + args[1] + "に変更しました");
         FileConfiguration config = CustomMods.getPlugin().getConfig();
-        config.set(args[0].toLowerCase(),args[1].toLowerCase());
+        if(args.length > 2 && (args[1].equalsIgnoreCase("sensing_radius") || args[1].equalsIgnoreCase("sensing_fire")))
+        {
+            if (args[1].equalsIgnoreCase("sensing_radius"))
+            {
+
+
+                String[] radius_array = args[2].split("-");
+                sender.sendMessage(ChatColor.GREEN + "ブロック破壊を無効にする対象の爆発の大きさ(sensing_radius)を" + String.join(",",radius_array) + "に設定しました");
+                config.set("sensing_radius",null);
+                int count=1;
+                for (String list:radius_array)
+                {
+                    config.set("sensing_radius.key" + count,new Float(list));
+                    count++;
+                }
+
+            }else if (args[1].equalsIgnoreCase("sensing_fire")) {
+                if (!(args[2].equalsIgnoreCase("all") || args[2].equalsIgnoreCase("false")  || args[2].equalsIgnoreCase("true")))
+                {
+                    sender.sendMessage(ChatColor.RED + "引数の3番目が間違っています。詳しくは /help setconfig を参照してください");
+                    return true;
+                }
+                sender.sendMessage(ChatColor.GREEN + "ブロック破壊を無効にする対象の火の爆発(sensing_fire)を" + args[2] + "に変更しました");
+                config.set("sensing_fire",args[2]);
+            }
+        }else{
+            sender.sendMessage(ChatColor.GREEN + args[0] + "に必要な権限を" + args[1] + "に変更しました");
+            config.set(args[0].toLowerCase(),args[1].toLowerCase());
+        }
+
+
+
         CustomMods.getPlugin().saveConfig();
-        Bukkit.reload();
+        CustomMods.getPlugin().reloadConfig();
+        //Bukkit.reload();
 
         return true;
     }
--- src/main/java/com/omochiserver/custommods/motion/MobsGuard.java
+++ src/main/java/com/omochiserver/custommods/motion/MobsGuard.java
@@ -1,11 +1,14 @@
 package com.omochiserver.custommods.motion;
 
 
+import com.omochiserver.custommods.CustomMods;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 public class MobsGuard implements Listener
 {
@@ -16,15 +19,33 @@ public class MobsGuard implements Listener
 
     {
 
-        if (!(e.getFire()) && e.getRadius()==3.0)
+
+        FileConfiguration config = CustomMods.getPlugin().getConfig();
+        boolean decision=false;
+
+        for (String key : config.getConfigurationSection("sensing_radius").getKeys(false)) {
+
+            if (String.valueOf(config.getDouble("sensing_radius." + key)).equalsIgnoreCase(String.valueOf(e.getRadius())))
+            {
+
+                if ((String.valueOf(e.getFire()).equalsIgnoreCase(config.getString("sensing_fire"))) || "all".equalsIgnoreCase(config.getString("sensing_fire")))
+                {
+                    decision=true;
+                }
+            }
+        }
+        if (decision)
         {
+            int power = (int)e.getRadius();
             e.setRadius(0);
             Location loc= e.getEntity().getLocation();
             double x=loc.getX();
             double y=loc.getY();
             double z=loc.getZ();
-            loc.getWorld().createExplosion(x,y,z,3,false,false);
+            loc.getWorld().createExplosion(x,y,z,power,false,false);
 
         }
     }
+
+
 }
