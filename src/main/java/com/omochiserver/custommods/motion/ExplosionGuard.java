4e78a0d4f074916910701e3d11dc021bf84138d1
hiro1234omochi <hiromochi01234@outlook.jp>
2022-09-11 10:21:56 +0900

    ・2.5-SNAPSHOT ・setconfigコマンドでall値を設定するとブロック破壊を無効にできなくなる不具合を修正。 ・onCreeperDoNotBreakBlockクラスの名前をonEntityDoNotBreakBlockに変更。 ・ベッドやリスポーンアンカーを爆発させたときにブロックが壊れてしまう不具合を修正。

--- src/main/java/com/omochiserver/custommods/motion/ExplosionGuard.java
+++ src/main/java/com/omochiserver/custommods/motion/ExplosionGuard.java
@@ -2,21 +2,22 @@ package com.omochiserver.custommods.motion;
 
 
 import com.omochiserver.custommods.CustomMods;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
 public class ExplosionGuard implements Listener
 {
 
     @EventHandler
 
-    public void onCreeperDoNotBreakBlock(ExplosionPrimeEvent e)
+    public void onEntityDoNotBreakBlock(ExplosionPrimeEvent e)
 
     {
-
-
         FileConfiguration config = CustomMods.getPlugin().getConfig();
         boolean decision=false;
         //エラーがあるか確認
@@ -30,14 +31,14 @@ public class ExplosionGuard implements Listener
         if (!(config.getString("sensing_fire").equalsIgnoreCase("true") || config.getString("sensing_fire").equalsIgnoreCase("false")
         || config.getString("sensing_fire").equalsIgnoreCase("all") || config.getString("sensing_fire").equalsIgnoreCase("null")))
         {
-            CustomMods.getPlugin().getLogger().info("config.ymlのsensing_fireタグの値が不正です。true,false,allから選択できます。");
+            CustomMods.getPlugin().getLogger().info("config.ymlのsensing_fireタグの値が不正です。true,false,all,null から選択できます。");
         }
         for (String key : config.getConfigurationSection("sensing_radius").getKeys(false)) {
 
             if (String.valueOf(config.getDouble("sensing_radius." + key)).equalsIgnoreCase(String.valueOf(e.getRadius())))
             {
 
-                if ((String.valueOf(e.getFire()).equalsIgnoreCase(config.getString("sensing_fire"))) || "all".equalsIgnoreCase(config.getString("sensing_fire")) && (!("all".equalsIgnoreCase(config.getString("sensing_fire")))))
+                if ((String.valueOf(e.getFire()).equalsIgnoreCase(config.getString("sensing_fire"))) || "all".equalsIgnoreCase(config.getString("sensing_fire")) && (!("null".equalsIgnoreCase(config.getString("sensing_fire")))))
                 {
                     decision=true;
                 }
@@ -53,6 +54,40 @@ public class ExplosionGuard implements Listener
             double z=loc.getZ();
             loc.getWorld().createExplosion(x,y,z,power,false,false);
 
+        }
+    }
+    @EventHandler
+    public void onBlockDoNotBreakBlock(BlockExplodeEvent e){
+        FileConfiguration config = CustomMods.getPlugin().getConfig();
+        boolean decision=false;
+        //エラーがあるか確認
+        for (String key : config.getConfigurationSection("sensing_radius").getKeys(false))
+        {
+            if (config.isString("sensing_radius." + key) || config.getDouble("sensing_radius." + key) >10 || config.getDouble("sensing_radius." + key) <1)
+            {
+                CustomMods.getPlugin().getLogger().info("config.ymlのsensing_radiusの" + key + "の値が不正です。1~10の間の少数または整数が指定できます。");
+            }
+        }
+        if (!(config.getString("sensing_fire").equalsIgnoreCase("true") || config.getString("sensing_fire").equalsIgnoreCase("false")
+                || config.getString("sensing_fire").equalsIgnoreCase("all") || config.getString("sensing_fire").equalsIgnoreCase("null")))
+        {
+            CustomMods.getPlugin().getLogger().info("config.ymlのsensing_fireタグの値が不正です。true,false,all,null から選択できます。");
+        }
+
+        int power=(int)(1/e.getYield());
+        for (String key : config.getConfigurationSection("sensing_radius").getKeys(false)) {
+            if ((int)(100/config.getDouble("sensing_radius." + key))==(int)(e.getYield()*100))
+            {
+                if ("true".equalsIgnoreCase(config.getString("sensing_fire")) || "all".equalsIgnoreCase(config.getString("sensing_fire")) && (!("null".equalsIgnoreCase(config.getString("sensing_fire")))))
+                {
+                    decision=true;
+                }
+            }
+        }
+        if (decision){
+            e.setCancelled(true);
+            Block block=e.getBlock();
+            block.getWorld().createExplosion(block.getX(),block.getY(),block.getZ(),power,false,false);
         }
     }
 
